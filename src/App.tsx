3ee01fd8a4d12014ae0aad338a1ad54e63e3c9ee
David Korpos <korposdavid@gmail.com>
2019-11-04 13:49:57 +0100

    remove unused code from generated files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return(
+        <div className="App">
+          <h2>hello world</h2>
+        </div>
+    );
+  };
+}
 
 export default App;
