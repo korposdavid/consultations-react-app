c37fa4911144682953a9f941790ffa69f802edca
David Korpos <korposdavid@gmail.com>
2019-12-04 16:03:16 +0100

    create LoginForm

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
+import Col from "react-bootstrap/Col";
+import UserModel from "../models/UserModel"
+
+interface Props {
+  username: string;
+}
+
+interface State {
+  username: string;
+  password: string;
+}
+
+export class LoginForm extends Component<Props, State> {
+  state: State = {
+    username: "",
+    password: ""
+  };
+
+  handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    axios({
+      method: "post",
+      url: "http://localhost:8080/auth/signin",
+      data: {
+        username: this.state.username,
+        password: this.state.password
+      }
+    }).then(response => {
+      //TODO: save logged in user
+      const responseToken = response.data.token;
+      const username = response.data.username;
+      
+    });
+  };
+
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ [e.target.name]: e.target.value } as Pick<State, any>);
+  };
+
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Row>
+          <Col md={2}>
+            <Form.Control
+              type="text"
+              onChange={this.handleChange}
+              name="username"
+              placeholder="Username"
+              required
+            />
+          </Col>
+          <Col md={2}>
+            <Form.Control
+              name="password"
+              type="password"
+              onChange={this.handleChange}
+              placeholder="Password"
+              required
+            />
+          </Col>
+          <Col md={1}>
+            <Button name="Login" variant="success" type="submit">
+              Login
+            </Button>
+          </Col>
+        </Form.Row>
+      </Form>
+    );
+  }
+}
+
+export default LoginForm;
